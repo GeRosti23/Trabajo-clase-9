2110d8f8fdfbd1645f4c551323b25342aa142906
Genaro <genarorosti@hotmail.com>
2022-09-08 21:45:32 -0300

    Initial commit

---
+++ js/variables.js
@@ -0,0 +1,12 @@
+//VARIABLES
+
+var pais = "España";
+var continente = "Europa";
+var antiguedad = "2021";
+var pais_y_continente = pais + "" + continente;
+
+pais = "Mexico";
+continente = "latinoamerica";
+
+console.log(pais, continente, antiguedad);
+alert(pais_y_continente);
